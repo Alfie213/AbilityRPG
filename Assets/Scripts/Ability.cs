77295cfec18a4ff630f412ca2fc3bc8db1b20e26
Alfie213 <arturbogoratov@gmail.com>
2024-10-13 19:12:52 +0400

    Update Ability.cs

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -10,16 +10,48 @@ public enum AbilityType
 public abstract class Ability
 {
     public abstract AbilityType Type { get; }
+    public abstract int InstantValue { get; set; }
     public abstract int Cooldown { get; set; }
 }
 
 public abstract class AbilityWithDuration : Ability
 {
     public abstract int Duration { get; set; }
+    public abstract int DurationValue { get; set; }
 }
 
-public class AbilityAttack : Ability
+public class Attack : Ability
 {
     public override AbilityType Type => AbilityType.Attack;
-    
+    public override int InstantValue { get; set; } = 8;
+    public override int Cooldown { get; set; } = 0;
+}
+
+public class Barrier : AbilityWithDuration
+{
+    public override AbilityType Type => AbilityType.Barrier;
+    public override int Cooldown { get; set; } = 4;
+    public override int Duration { get; set; } = 2;
+    public override int InstantValue { get; set; }
+    public override int DurationValue { get; set; } = 0;
+}
+
+public class Regeneration : AbilityWithDuration
+{
+    public override AbilityType Type => AbilityType.Regeneration;
+    public override int Cooldown { get; set; } = 5;
+    public override int Duration { get; set; } = 3;
+}
+
+public class Fireball : AbilityWithDuration
+{
+    public override AbilityType Type => AbilityType.Fireball;
+    public override int Cooldown { get; set; } = 6;
+    public override int Duration { get; set; } = 5;
+}
+
+public class Cleanse : Ability
+{
+    public override AbilityType Type => AbilityType.Cleanse;
+    public override int Cooldown { get; set; } = 5;
 }
