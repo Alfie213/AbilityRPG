ed2761d87a0736fcdcbabe673c07dea1ef59da0b
Alfie213 <arturbogoratov@gmail.com>
2024-10-13 21:19:52 +0400

    Rename

--- Assets/Scripts/AbilityBase.cs
+++ Assets/Scripts/AbilityBase.cs
@@ -7,44 +7,44 @@ public enum AbilityType
     Cleanse
 }
 
-public abstract class Ability
+public abstract class AbilityBase
 {
     public abstract AbilityType Type { get; }
     public abstract int Cooldown { get; set; }
 }
 
-public abstract class AbilityAttack : Ability
+public abstract class AbilityAttackBase : AbilityBase
 {
     public abstract int AttackValue { get; set; }
 }
 
-public class Attack : AbilityAttack
+public class AttackBase : AbilityAttackBase
 {
     public override AbilityType Type => AbilityType.Attack;
     public override int Cooldown { get; set; } = 0;
     public override int AttackValue { get; set; } = 8;
 }
 
-public class Barrier : Ability
+public class Barrier : AbilityBase
 {
     public override AbilityType Type => AbilityType.Barrier;
     public override int Cooldown { get; set; } = 4;
 }
 
-public class Regeneration : Ability
+public class Regeneration : AbilityBase
 {
     public override AbilityType Type => AbilityType.Regeneration;
     public override int Cooldown { get; set; } = 5;
 }
 
-public class Fireball : AbilityAttack
+public class Fireball : AbilityAttackBase
 {
     public override AbilityType Type => AbilityType.Fireball;
     public override int Cooldown { get; set; } = 6;
     public override int AttackValue { get; set; } = 5;
 }
 
-public class Cleanse : Ability
+public class Cleanse : AbilityBase
 {
     public override AbilityType Type => AbilityType.Cleanse;
     public override int Cooldown { get; set; } = 5;
