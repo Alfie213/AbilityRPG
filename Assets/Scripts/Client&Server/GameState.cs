4edfd8b357c3550129cfcd1dd4a570e51a7f6ffd
Alfie213 <arturbogoratov@gmail.com>
2024-10-14 17:32:41 +0400

    Update GameState.cs

--- Assets/Scripts/Client&Server/GameState.cs
+++ Assets/Scripts/Client&Server/GameState.cs
@@ -2,8 +2,8 @@ using System.Collections.Generic;
 
 public class GameState
 {
-    public int PlayerHealth;
-    public int EnemyHealth;
-    public List<EffectBase> PlayerEffects;
-    public List<EffectBase> EnemyEffects;
+    public int PlayerHealth = 100;
+    public int EnemyHealth = 100;
+    public readonly List<EffectBase> PlayerEffects = new();
+    public readonly List<EffectBase> EnemyEffects = new();
 }
