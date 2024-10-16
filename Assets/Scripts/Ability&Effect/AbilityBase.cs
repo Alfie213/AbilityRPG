32c6b5d8e3d1844b7caa671915dad33970ceba49
Alfie213 <arturbogoratov@gmail.com>
2024-10-16 05:42:32 +0400

    Refactoring

--- Assets/Scripts/Ability&Effect/AbilityBase.cs
+++ Assets/Scripts/Ability&Effect/AbilityBase.cs
@@ -19,43 +19,43 @@ public abstract class AbilityBase
     // }
     
     public abstract AbilityType Type { get; }
-    public abstract int MaxCooldown { get; set; }
+    public abstract int MaxCooldown { get; }
     public int CurrentCooldown { get; set; }
 }
 
 public abstract class AbilityAttackBase : AbilityBase
 {
-    public abstract int AttackValue { get; set; }
+    public abstract int AttackValue { get; }
 }
 
 public class AbilityAttack : AbilityAttackBase
 {
     public override AbilityType Type => AbilityType.Attack;
-    public override int MaxCooldown { get; set; } = 0;
-    public override int AttackValue { get; set; } = 8;
+    public override int MaxCooldown => 0;
+    public override int AttackValue => 8;
 }
 
 public class AbilityBarrier : AbilityBase
 {
     public override AbilityType Type => AbilityType.Barrier;
-    public override int MaxCooldown { get; set; } = 4;
+    public override int MaxCooldown => 4;
 }
 
 public class AbilityRegeneration : AbilityBase
 {
     public override AbilityType Type => AbilityType.Regeneration;
-    public override int MaxCooldown { get; set; } = 5;
+    public override int MaxCooldown => 5;
 }
 
 public class AbilityFireball : AbilityAttackBase
 {
     public override AbilityType Type => AbilityType.Fireball;
-    public override int MaxCooldown { get; set; } = 6;
-    public override int AttackValue { get; set; } = 5;
+    public override int MaxCooldown => 6;
+    public override int AttackValue => 5;
 }
 
 public class AbilityCleanse : AbilityBase
 {
     public override AbilityType Type => AbilityType.Cleanse;
-    public override int MaxCooldown { get; set; } = 5;
+    public override int MaxCooldown => 5;
 }
