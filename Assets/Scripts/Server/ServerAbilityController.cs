796ba7f4095a99ab3de282a28a31195b844e4b85
Alfie213 <arturbogoratov@gmail.com>
2024-10-18 06:22:04 +0400

    Update ServerAbilityController.cs

--- Assets/Scripts/Server/ServerAbilityController.cs
+++ Assets/Scripts/Server/ServerAbilityController.cs
@@ -10,29 +10,11 @@ public class ServerAbilityController
     public bool TrySubmitPlayerAbilityUsage(GameState gameState, AbilityType abilityType)
     {
         AbilityBase ability = gameState.Player.Abilities[abilityType];
-        
+
         if (!ability.IsReady)
             return false;
 
-        Player target;
-        
-        switch (abilityType)
-        {
-            case AbilityType.Attack:
-            case AbilityType.Fireball:
-                target = gameState.Enemy;
-                break;
-
-            case AbilityType.Regeneration:
-            case AbilityType.Barrier:
-            case AbilityType.Cleanse:
-                target = gameState.Player;
-                break;
-
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-        
+        Player target = GetTarget(gameState, abilityType, isPlayer: true);
         ability.Cast(target);
 
         Debug.Log("<color=green>Player</color> Ability casted: " + abilityType);
@@ -45,53 +27,40 @@ public class ServerAbilityController
             .Where(entry => entry.Value.IsReady)
             .Select(entry => entry.Key)
             .ToList();
-        // Debug.Log($"After this turn <color=red>Enemy</color> will have {availableAbilities.Count} abilities.");
-        
+
         if (availableAbilities.Count > 0)
         {
             AbilityType randomAbilityType = availableAbilities[Random.Next(availableAbilities.Count)];
             SubmitEnemyAbilityUsage(gameState, randomAbilityType);
         }
         else
+        {
             throw new NotImplementedException();
+        }
     }
     
     private void SubmitEnemyAbilityUsage(GameState gameState, AbilityType abilityType)
     {
         AbilityBase ability = gameState.Enemy.Abilities[abilityType];
-
-        Player target;
-        
-        switch (abilityType)
-        {
-            case AbilityType.Attack:
-            case AbilityType.Fireball:
-                target = gameState.Player;
-                break;
-
-            case AbilityType.Regeneration:
-            case AbilityType.Barrier:
-            case AbilityType.Cleanse:
-                target = gameState.Enemy;
-                break;
-
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-        
+        Player target = GetTarget(gameState, abilityType, isPlayer: false);
         ability.Cast(target);
 
         Debug.Log("<color=red>Enemy</color> Ability casted: " + abilityType);
     }
 
+    private Player GetTarget(GameState gameState, AbilityType abilityType, bool isPlayer)
+    {
+        return abilityType switch
+        {
+            AbilityType.Attack or AbilityType.Fireball => isPlayer ? gameState.Enemy : gameState.Player,
+            AbilityType.Regeneration or AbilityType.Barrier or AbilityType.Cleanse => isPlayer ? gameState.Player : gameState.Enemy,
+            _ => throw new ArgumentOutOfRangeException(nameof(abilityType), "Unknown ability type")
+        };
+    }
+
     public void ReduceCurrentCooldown(GameState gameState)
     {
-        foreach (AbilityBase ability in gameState.AllAbilities)
-        {
-            if (ability.CurrentCooldown < 0)
-                continue;
-
+        foreach (AbilityBase ability in gameState.AllAbilities.Where(ability => ability.CurrentCooldown >= 0))
             ability.CurrentCooldown--;
-        }
     }
 }
